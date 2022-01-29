bf4a2f62386545f5b4a0dd0b9a69f5a7f040bbf1
Raphael Ouzilleau <rapahel.ouzilleau@gmail.com>
2022-01-29 01:07:31 -0500

    Ajout des icones de base de font-awesome.

--- ritzkick/components/Icons.js
+++ ritzkick/components/Icons.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faCheckSquare, faShoppingCart, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faCheckSquare, faShoppingCart, faChevronDown, faChartLine, faWallet, faGlobe, faCoins, faUserCircle, faMedal, faInfoCircle, faQuestionCircle, faTicketAlt, faHandsHelping, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const Icons = {
     Cart: () => { return(<FontAwesomeIcon className='icon' icon={faShoppingCart}/>) },
-    ChevronDown: () => { return(<FontAwesomeIcon className='big-icon' icon={faChevronDown}/>) }
+    ChevronDown: () => { return(<FontAwesomeIcon className='big-icon' icon={faChevronDown}/>) },
+    ChartLine: () => { return(<FontAwesomeIcon className='icon' icon={faChartLine}/>) },
+    Wallet: () => { return(<FontAwesomeIcon className='icon' icon={faWallet}/>) },
+    Globe: () => { return(<FontAwesomeIcon className='icon' icon={faGlobe}/>) },
+    Coins: () => { return(<FontAwesomeIcon className='icon' icon={faCoins}/>) },
+    UserCircle: () => { return(<FontAwesomeIcon className='icon' icon={faUserCircle}/>) },
+    Medal: () => { return(<FontAwesomeIcon className='icon' icon={faMedal}/>) },
+    Bars: () => { return(<FontAwesomeIcon className='icon' icon={faBars}/>) },
+    Times: () => { return(<FontAwesomeIcon className='icon' icon={faTimes}/>) },
+    InfoCircle: () => { return(<FontAwesomeIcon className='icon' icon={faInfoCircle}/>) },
+    QuestionCircle: () => { return(<FontAwesomeIcon className='icon' icon={faQuestionCircle}/>) },
+    Ticket: () => { return(<FontAwesomeIcon className='icon' icon={faTicketAlt}/>) }
 }
 
 export default Icons;
--- ritzkick/pages/index.js
+++ ritzkick/pages/index.js
@@ -4,9 +4,9 @@ import Footer from '../components/Footer'
 import Icons from '../components/Icons'
 import BottomArrow from '../components/BottomArrow'
 
-const TITLE = "KRYPTOW"
-const SUB_TTTLE = "Forget those crapy crypto wallet"
-const SUB_TITLE_2 = 'Get your keys safe!'
+const TITLE = "CRYPTOOL"
+const SUB_TTTLE = "Restez à l'affut de vos cryptos favorites"
+// <div className="particletext bubbles row"></div>
 export default function Home() {
   return (
     <div>
@@ -16,20 +16,15 @@ export default function Home() {
         <section className='section row principal center'>
           <div className='column center'>
             <div className='column center'>
-              <div className='website-title item'>
-                {TITLE}
-                <div className="particletext bubbles row"></div>
-              </div>
+              <h1 className='website-title item'>{TITLE}</h1>
               <h2 className='subtitle item'>{SUB_TTTLE}</h2>
-              <h3 className='subtitle item'>{SUB_TITLE_2}</h3>
             </div>
           </div>
         </section>
         <BottomArrow/>        
       </main>
 
-      <svg className='svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fillOpacity="1" d="M0,96L17.1,85.3C34.3,75,69,53,103,64C137.1,75,171,117,206,117.3C240,117,274,75,309,96C342.9,117,377,203,411,218.7C445.7,235,480,181,514,144C548.6,107,583,85,617,74.7C651.4,64,686,64,720,80C754.3,96,789,128,823,149.3C857.1,171,891,181,926,192C960,203,994,213,1029,197.3C1062.9,181,1097,139,1131,117.3C1165.7,96,1200,96,1234,101.3C1268.6,107,1303,117,1337,144C1371.4,171,1406,213,1423,234.7L1440,256L1440,320L1422.9,320C1405.7,320,1371,320,1337,320C1302.9,320,1269,320,1234,320C1200,320,1166,320,1131,320C1097.1,320,1063,320,1029,320C994.3,320,960,320,926,320C891.4,320,857,320,823,320C788.6,320,754,320,720,320C685.7,320,651,320,617,320C582.9,320,549,320,514,320C480,320,446,320,411,320C377.1,320,343,320,309,320C274.3,320,240,320,206,320C171.4,320,137,320,103,320C68.6,320,34,320,17,320L0,320Z"></path></svg>
-        
+      <svg className='svg svg-transit' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,128L205.7,192L411.4,128L617.1,192L822.9,32L1028.6,64L1234.3,192L1440,96L1440,320L1234.3,320L1028.6,320L822.9,320L617.1,320L411.4,320L205.7,320L0,320Z"></path></svg> 
       <section className='section row second'>
         <div className='division'>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error repellendus reiciendis optio odit dolores blanditiis esse amet veniam itaque excepturi dolorem quas recusandae, eligendi voluptas, ut tempore assumenda et tempora?</p>
