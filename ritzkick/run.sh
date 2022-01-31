f38e76de752a3e7b1860d4471ad8a070b677a1cf
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-01-31 15:23:17 -0500

    Removing image before restarting

--- ritzkick/run.sh
+++ ritzkick/run.sh
@@ -2,6 +2,8 @@ docker stop ritzkick
 
 docker container prune -f
 
+docker image prune -f
+
 docker build -t  img_ritzkick -f ./Dockerfile .
 
 docker run -d -p 3000:3000 --name ritzkick img_ritzkick
