3a22778b95c4e162d1cd21ca617864845e7c51de
Raphael Ouzilleau <rapahel.ouzilleau@gmail.com>
2022-01-31 13:16:40 -0500

    barre de navigation terminee

--- ritzkick/components/Navbar.js
+++ ritzkick/components/Navbar.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
+import { useRouter } from "next/router";
 import Icons from './Icons';
 //import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar(props) {
+    const router = useRouter();
+    
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
@@ -17,32 +20,69 @@ function Navbar() {
         return () => window.removeEventListener('scroll', onScroll);
     }, []);
    
-/* <Link to='' className='navbar-logo'>Revolv'Air</Link> */
-// <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
     return (
-        <>  
+        <>
+        
         <nav className='navbar' id='nav'>
-            <div className='navbar-container'>
-                <div className={isScrolled ? 'lil-nav scrolled' : 'lil-nav'}>
-                    <div className='menu-icon' onClick={handleClick}>
-                        {click ? <Icons.Times/> : <Icons.Bars/>}
+            {props.mobile ? (
+                <>
+                    <div className='lil-nav'>
+                        <div className='menu-icon' onClick={handleClick}>
+                            {click ? <Icons.Times/> : <Icons.Bars/>}
+                        </div>
                     </div>
+                </>
+            ) : (
+                <></>
+            )}
+            <div className={props.mobile ? (click ? 'active navbar-container-mobile': 'hidden') :'navbar-container'}>
+                    <ul className={props.mobile ? 'nav-menu-mobile' : 'nav-menu' }>
+                        <li className={router.pathname == '/' ? 'nav-item active-link' : 'nav-item'}>
+                            <Icons.Home/>
+                            <a href="/" className='nav-links' onClick={closeMobileMenu}>Home</a>
+                        </li>
+                        <li className={router.pathname == '/assets' ? 'nav-item active-link' : 'nav-item'}>
+                            <Icons.Coins/>
+                            <a href="/assets" className='nav-links' onClick={closeMobileMenu}>Assets</a>
+                        </li>
+                        <li className={router.pathname == '/podium' ? 'nav-item active-link' : 'nav-item'}>
+                            <Icons.Medal/>
+                            <a href="/podium" className='nav-links' onClick={closeMobileMenu}>Podium</a>
+                        </li>
+                        <li className={router.pathname == '/help' ? 'nav-item active-link' : 'nav-item'}>
+                            <Icons.InfoCircle/>
+                            <a href="/help" className='nav-links'  onClick={closeMobileMenu}>Help Center</a>
+                        </li>
+                    </ul>
+                    <ul className={props.mobile ? 'nav-menu-mobile' : 'nav-menu' }>
+                        { props.connected ? (
+                            <>
+                                <li className={router.pathname == '/wallet' ? 'nav-item active-link' : 'nav-item'}>
+                                    <Icons.Wallet/>
+                                    <a href="/wallet" className='nav-links'  onClick={closeMobileMenu}>Wallet</a>
+                                </li>
+                                <li className={router.pathname == '/profile' ? 'nav-item active-link' : 'nav-item'}>
+                                    <Icons.UserCircle/>
+                                    <a href="/profile" className='nav-links'  onClick={closeMobileMenu}>Profile</a>
+                                </li>
+                                <li className={router.pathname == '/logout' ? 'nav-item active-link' : 'nav-item'}>
+                                    <Icons.DoorOpen/>
+                                    <a href="/logout" className='nav-links'  onClick={closeMobileMenu}>Log out</a>
+                                </li>
+                            </>
+                        ): (
+                            <>
+                                <li className={router.pathname == '/login' ? 'nav-item active-link' : 'nav-item'}>
+                                    <div className='nav-icons'><Icons.DoorClosed/></div>
+                                    <a href="/login" className='nav-links'  onClick={closeMobileMenu}>Log in</a>
+                                </li>
+                                <li className={router.pathname == '/register' ? 'nav-item active-link' : 'nav-item'}>
+                                    <a href="/register" className='nav-links button'  onClick={closeMobileMenu}>Register</a>
+                                </li>
+                            </>
+                        )}
+                    </ul>
                 </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <a href="#home" className='nav-links' onClick={closeMobileMenu}>HOME</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a href="#about" className='nav-links' onClick={closeMobileMenu}>ABOUT</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a href="#projects" className='nav-links' onClick={closeMobileMenu}>PROJECTS</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a href="#contact" className='nav-links'  onClick={closeMobileMenu}>CONTACT</a>
-                    </li>
-                </ul>
-            </div>
         </nav>
         </>
     )
