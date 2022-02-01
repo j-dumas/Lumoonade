66f95f6277095d11d9d91a4db5b62daf5c87f11b
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-01 16:00:01 +0000

    __dirname in fullchain path

--- ritzkick/server.js
+++ ritzkick/server.js
@@ -27,9 +27,8 @@ const ssl = process.env.SSL || false
 const httpsOptions = {}
 if (ssl == 'true') {
 	log.info('SERVER', 'Reading certificates')
-	log.debug('SERVER', __filename)
 	httpsOptions.key = fs.readFileSync(`${__dirname}/certificates/privkey.pem`)
-	httpsOptions.cert = fs.readFileSync('./certificates/fullchain.pem')
+	httpsOptions.cert = fs.readFileSync(`${__dirname}/certificates/fullchain.pem`)
 }
 
 const app = next({ dev })
