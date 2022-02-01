128dc07c4babc0ee95130f2c0e1e0c0c8eb1a051
Raphael Ouzilleau <rapahel.ouzilleau@gmail.com>
2022-02-01 14:42:37 -0500

    Modif Simple Crypto View

--- ritzkick/components/SimpleCryptoView.js
+++ ritzkick/components/SimpleCryptoView.js
@@ -12,18 +12,18 @@ function SimpleCryptoView(props) {
 
     return (
         <>
-            <div className='simple-crypto-view row h-center'>
-            <div className='simple-crypto-view-item row left h-center'>
-                <img className='simple-crypto-view-logo' src={data.abbreviation + ".svg"} alt="" />
-                <div className='column simple-crypto-names'>
-                    <p className='simple-crypto-name'>{data.name}</p>
-                    <p className='simple-crypto-abbreviation'>{data.abbreviation}</p>
+            <a href={'assets/'+data.abbreviation} className='simple-crypto-view row h-center'>
+                <div className='simple-crypto-view-item row left h-center'>
+                    <img className='simple-crypto-view-logo' src={data.abbreviation + ".svg"} alt="" />
+                    <div className='column simple-crypto-names'>
+                        <p className='simple-crypto-name'>{data.name}</p>
+                        <p className='simple-crypto-abbreviation'>{data.abbreviation}</p>
+                    </div>
                 </div>
-            </div>
                 <p className='simple-crypto-view-item simple-crypto-price'>{data.price}</p>
                 <p className={change>0?'simple-crypto-view-item simple-crypto-change c-green': change==0?'simple-crypto-view-item simple-crypto-change c-white':'simple-crypto-view-item simple-crypto-change c-red'}>{change} %</p>
                 <SimpleChart data={data}/>
-            </div>
+            </a>
         </>
     )
 }
