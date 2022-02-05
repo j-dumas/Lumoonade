394021a7971263509e42f4192aa938c43e90a758
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-05 01:29:35 +0000

    Public ENV for Client

--- ritzkick/pages/_app.js
+++ ritzkick/pages/_app.js
@@ -25,18 +25,17 @@ config.autoAddCss = false /* eslint-disable import/first */
 
 function MyApp({ Component, pageProps }) {
 	let app
-	console.log(`Prod: ${process.env.NEXT_PUBLIC_PROD}`)
 	if (process.env.NEXT_PUBLIC_PROD == 'true') {
 		app = (
 			<>
-				<Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_TOKEN}`} />
+				<Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TOKEN}`} />
 				<Script strategy='lazyOnload'>
 					{
 						`window.dataLayer = window.dataLayer || [];
 						function gtag(){dataLayer.push(arguments);}
 						gtag('js', new Date());
 
-						gtag('config', '${process.env.GOOGLE_ANALYTICS_TOKEN}');`
+						gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TOKEN}');`
 					}
 				</Script>
 				<Component {...pageProps} />
