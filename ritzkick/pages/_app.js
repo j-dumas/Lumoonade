fafb757a8fe10593d450c27f53b738136dac42d6
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-05 00:58:51 +0000

    Added Google Analytics

--- ritzkick/pages/_app.js
+++ ritzkick/pages/_app.js
@@ -14,6 +14,8 @@ import '../styles/BubbleEffect.css'
 import '../styles/Charts.css'
 import '../styles/SimpleCrypto.css'
 
+import Script from 'next/script'
+
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -21,8 +23,34 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false /* eslint-disable import/first */
 
+const prod = process.env.PROD || false
+const gToken = process.env.GOOGLE_ANALYTICS_TOKEN || null
+
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	let app
+	if (prod == 'true') {
+		app = (
+			<>
+				<Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${gToken}`} />
+				<Script strategy='lazyOnload'>
+					{
+						`window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+
+						gtag('config', '${gToken}');`
+					}
+				</Script>
+				<Component {...pageProps} />
+			</>
+		)
+	} else {
+		app = (
+			<Component {...pageProps} />
+		)
+	}
+
+	return app
 }
 
 export default MyApp
