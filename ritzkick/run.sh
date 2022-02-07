3f16b375e5c8b086f5e5c4caffae5617bede0930
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-07 14:24:45 -0500

    Rotating logs to make space

--- ritzkick/run.sh
+++ ritzkick/run.sh
@@ -8,5 +8,5 @@ docker image prune -f
 docker build -t  img_ritzkick -f ./docker/Dockerfile .
 docker build -t  img_ritzkick_test -f ./docker/DockerfileTest .
 
-docker run -d -p 443:443 --name ritzkick img_ritzkick
-docker run -d -p 3000:3000 --name ritzkick-test img_ritzkick_test
+docker run --log-opt max-size=100m --log-opt max-file=5 -d -p 443:443 --name ritzkick img_ritzkick
+docker run --log-opt max-size=100m --log-opt max-file=5 -d -p 3000:3000 --name ritzkick-test img_ritzkick_test
