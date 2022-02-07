c7b528a0ae55eaa5f19a466338d8ab3116450721
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-07 12:13:53 -0500

    Changing open ports

--- ritzkick/run.sh
+++ ritzkick/run.sh
@@ -9,4 +9,4 @@ docker build -t  img_ritzkick -f ./docker/Dockerfile .
 docker build -t  img_ritzkick_test -f ./docker/DockerfileTest .
 
 docker run -d -p 443:443 --name ritzkick img_ritzkick
-docker run -d -p 80:80 --name ritzkick-test img_ritzkick_test
+docker run -d -p 3000:3000 --name ritzkick-test img_ritzkick_test
