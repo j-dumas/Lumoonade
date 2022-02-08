7bea921dba42aa9446bc2999cecd52ac8a82e7a7
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-08 14:07:18 -0500

    Back to old img

--- ritzkick/components/Bubbles.js
+++ ritzkick/components/Bubbles.js
@@ -1,57 +1,57 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
-import Image from 'next/image'
 
 //https://cryptologos.cc/
 
 function Bubbles() {
 	return (
 		<div>
-			<Image
+			<img
 				className='bubble x1'
 				src='https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=018'
 				alt=''
 			/>
-			<Image
+			<img
 				className='bubble x2'
 				src='https://cryptologos.cc/logos/tether-usdt-logo.svg?v=018'
 				alt=''
 			/>
-			<Image
+			<img
 				className='bubble x3'
 				src='https://cryptologos.cc/logos/binance-coin-bnb-logo.svg?v=018'
 				alt=''
 			/>
-			<Image
+			<img
 				className='bubble x4'
 				src='https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=018'
 				alt=''
 			/>
-			<Image
+			<img
 				className='bubble x5'
 				src='https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=018'
 				alt=''
 			/>
-			<Image
+			<img
 				className='bubble x6'
 				src='https://cryptologos.cc/logos/tether-usdt-logo.svg?v=018'
 				alt=''
 			/>
-			<Image
+			<img
 				className='bubble x7'
 				src='https://cryptologos.cc/logos/dogecoin-doge-logo.svg?v=018'
 				alt=''
 			/>
-			<Image
+			<img
 				className='bubble x8'
 				src='https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=018'
 				alt=''
 			/>
-			<Image
+			<img
 				className='bubble x9'
 				src='https://cryptologos.cc/logos/dogecoin-doge-logo.svg?v=018'
 				alt=''
 			/>
-			<Image
+			<img
 				className='bubble x10'
 				src='https://cryptologos.cc/logos/shiba-inu-shib-logo.svg?v=018'
 				alt=''
