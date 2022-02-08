9de10427f0ab5653d8f0e76ed32c1ed2d6efcf31
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-08 16:41:34 -0500

    Renaming

--- ritzkick/components/Layout.js
+++ ritzkick/components/Layout.js
@@ -1,13 +1,13 @@
 import dynamic from 'next/dynamic'
 
-const Head = dynamic(() => import('./DomHead'))
+const DomHead = dynamic(() => import('./DomHead'))
 const Header = dynamic(() => import('./Header'))
 const Footer = dynamic(() => import('./Footer'))
 
 export default function Layout({ children, pageMeta }) {
 	return (
 		<>
-			<Head pageMeta={pageMeta} />
+			<DomHead pageMeta={pageMeta} />
 			<Header />
 			{children}
 			<Footer />
