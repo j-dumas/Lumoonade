d309e075736cd18cd4da40da35cc50359b50e6b2
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-08 10:22:09 -0500

    Back to port 80 for testing

--- ritzkick/run.sh
+++ ritzkick/run.sh
@@ -9,4 +9,4 @@ docker build -t  img_ritzkick -f ./docker/Dockerfile .
 docker build -t  img_ritzkick_test -f ./docker/DockerfileTest .
 
 docker run --log-opt max-size=100m --log-opt max-file=5 -d -p 443:443 --name ritzkick img_ritzkick
-docker run --log-opt max-size=100m --log-opt max-file=5 -d -p 3000:3000 --name ritzkick-test img_ritzkick_test
+docker run --log-opt max-size=100m --log-opt max-file=5 -d -p 80:80 --name ritzkick-test img_ritzkick_test
