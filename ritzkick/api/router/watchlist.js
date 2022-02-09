34c0906d7285250b3cc8d000532312f1cc05ab81
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-09 14:35:14 -0500

    create

--- ritzkick/api/router/watchlist.js
+++ ritzkick/api/router/watchlist.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Watchlist = require('../../db/model/watchlist')
+const authentification = require('../middleware/auth')
 const router = express.Router()
 
-router.get('/api/watchlists', async (req, res) => {
+router.post('/api/watchlist', authentification, async (req, res) => {
 	try {
-		const watchlists = await Watchlist.find({})
-		if (!watchlists || watchlists.length === 0) {
-			throw new Error('Unable to find watchlists.')
+		let data = {
+			owner: req.user._id,
+			...req.body,
 		}
-
-		res.send(watchlists)
+		const watchlist = new Watchlist(data)
+		await watchlist.save()
+		res.send(watchlist)
 	} catch (e) {
-		res.status(500).send({
-			message: e.message,
+		res.status(400).send({
+			error: e.message,
 		})
 	}
 })
