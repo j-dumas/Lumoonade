c7fe674abef643ff351d5246fc37f8b38e8dd3a2
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-10 11:03:09 -0500

    Checking if already exists

--- ritzkick/api/router/watchlist.js
+++ ritzkick/api/router/watchlist.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const Watchlist = require('../../db/model/watchlist')
 const authentification = require('../middleware/auth')
 const router = express.Router()
+const HttpError = require('../http_error')
 
 router.post('/api/watchlist', authentification, async (req, res) => {
 	try {
@@ -10,11 +11,15 @@ router.post('/api/watchlist', authentification, async (req, res) => {
 			owner: req.user._id,
 			...req.body,
 		}
+		const obj = Watchlist.findOne({ slug: req.body.slug, target: req.body.target })
+		if (obj) {
+			throw new HttpError('Already created', 409)
+		}
 		const watchlist = new Watchlist(data)
 		await watchlist.save()
-		res.send(watchlist)
+		res.status(201).send(watchlist)
 	} catch (e) {
-		res.status(400).send({
+		res.status(e.status).send({
 			error: e.message,
 		})
 	}
