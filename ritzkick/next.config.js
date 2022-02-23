7692f78665afe56607b60693d6cd49d439813f80
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-23 15:03:26 -0500

    Basic Security Headers

--- ritzkick/next.config.js
+++ ritzkick/next.config.js
@@ -4,7 +4,35 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const { i18n } = require('./next-i18next.config')
 
+const securityHeaders = [
+	{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on'
+	},
+	{
+		key: 'Strict-Transport-Security',
+		value: 'max-age=63072000; includeSubDomains; preload'
+	},
+	{
+		key: 'X-XSS-Protection',
+		value: '1; mode=block'
+	},
+	{
+		key: 'X-Frame-Options',
+		value: 'SAMEORIGIN'
+	}
+]
+
 module.exports = withBundleAnalyzer({
 	i18n,
-	reactStrictMode: true
+	reactStrictMode: true,
+	async headers() {
+		return [
+			{
+				// Apply these headers to all routes in your application.
+				source: '/:path*',
+				headers: securityHeaders
+			}
+		]
+	}
 })
