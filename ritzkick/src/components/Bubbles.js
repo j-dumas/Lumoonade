54fcd8f292c229058e947546ca48d5a210ee0c32
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-02-28 14:19:21 -0500

    SVGR for svgs

--- ritzkick/src/components/Bubbles.js
+++ ritzkick/src/components/Bubbles.js
@@ -1,22 +1,28 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react'
+
+import EthLogo from 'img/bubbles/ethereum-eth-logo.svg'
+import TetherLogo from 'img/bubbles/tether-usdt-logo.svg'
+import BinanceLogo from 'img/bubbles/binance-coin-bnb-logo.svg'
+import BtcLogo from 'img/bubbles/bitcoin-btc-logo.svg'
+import DogeLogo from 'img/bubbles/dogecoin-doge-logo.svg'
+import ShibaLogo from 'img/bubbles/shiba-inu-shib-logo.svg'
 
 //https://cryptologos.cc/
 
 function Bubbles() {
 	return (
-		<div>
-			<img className="bubble x1" src="/bubbles/ethereum-eth-logo.svg" alt="" />
-			<img className="bubble x2" src="/bubbles/tether-usdt-logo.svg" alt="" />
-			<img className="bubble x3" src="/bubbles/binance-coin-bnb-logo.svg" alt="" />
-			<img className="bubble x4" src="/bubbles/bitcoin-btc-logo.svg" alt="" />
-			<img className="bubble x5" src="/bubbles/ethereum-eth-logo.svg" alt="" />
-			<img className="bubble x6" src="/bubbles/tether-usdt-logo.svg" alt="" />
-			<img className="bubble x7" src="/bubbles/dogecoin-doge-logo.svg" alt="" />
-			<img className="bubble x8" src="/bubbles/bitcoin-btc-logo.svg" alt="" />
-			<img className="bubble x9" src="/bubbles/dogecoin-doge-logo.svg" alt="" />
-			<img className="bubble x10" src="/bubbles/shiba-inu-shib-logo.svg" alt="" />
-		</div>
+		<>
+			<EthLogo className="bubble x1" alt="" />
+			<TetherLogo className="bubble x2" alt="" />
+			<BinanceLogo className="bubble x3" alt="" />
+			<BtcLogo className="bubble x4" alt="" />
+			<EthLogo className="bubble x5" />
+			<TetherLogo className="bubble x6" alt="" />
+			<DogeLogo className="bubble x7" alt="" />
+			<BtcLogo className="bubble x8" alt="" />
+			<DogeLogo className="bubble x9" alt="" />
+			<ShibaLogo className="bubble x10" alt="" />
+		</>
 	)
 }
 
