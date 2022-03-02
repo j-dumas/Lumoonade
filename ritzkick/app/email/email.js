fd7758f29df9923dc120566b0547ecc855cd6503
unknown <1842251@csfoy.ca>
2022-03-02 12:27:51 -0500

    email validator (do not send on fake)

--- ritzkick/app/email/email.js
+++ ritzkick/app/email/email.js
@@ -1,25 +1,30 @@
 const sendgrid = require('@sendgrid/mail')
 const logger = require('../../utils/logging')
+const ev = require('../../utils/email-validator')
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendConfirmationEmail = (to, link) => {
-	sendgrid
-		.send({
-			to,
-			from: process.env.SENDGRID_EMAIL_SENDER,
-			subject: 'Welcome abord !',
-			html: `
-            	<h1>Cryptool Service</h1>
-				<hr>
-				<p>Hi,</p>
-            	<p>Please confirm your email at <a href="${link}">this</a> url.</p>
-        	`
-		})
-		.then((res) => {
-			logger.info('Email', `Email sent to ${to}!`)
-		})
-		.catch((_) => {})
+	ev.isLegitimateEmail(to).then((res) => {
+		if (res) {
+			sendgrid
+			.send({
+				to,
+				from: process.env.SENDGRID_EMAIL_SENDER,
+				subject: 'Welcome abord !',
+				html: `
+					<h1>Cryptool Service</h1>
+					<hr>
+					<p>Hi,</p>
+					<p>Please confirm your email at <a href="${link}">this</a> url.</p>
+				`
+			})
+			.then((_) => {
+				logger.info('Email', `Email sent to ${to}!`)
+			})
+			.catch((_) => {})
+		}
+	}).catch((_) => {})
 }
 
 /**
@@ -27,20 +32,23 @@ const sendConfirmationEmail = (to, link) => {
  * @param {string} to
  */
 const sendResetPasswordEmail = (to, link) => {
-	sendgrid
-		.send({
-			to,
-			from: process.env.SENDGRID_EMAIL_SENDER,
-			subject: 'Reset Password',
-			html: `
-            <h1>Cryptool Service</h1>
-            <p>click this link ! <a href="${link}">reset</a></p>
-        `
-		})
-		.then((res) => {
-			logger.info('Email', `Email sent to ${to}!`)
-		})
-		.catch((_) => {})
+	ev.isLegitimateEmail(to).then((res) => {
+		if (res) {
+			sendgrid.send({
+				to,
+				from: process.env.SENDGRID_EMAIL_SENDER,
+				subject: 'Reset Password',
+				html: `
+					<h1>Cryptool Service</h1>
+					<p>click this link ! <a href="${link}">reset</a></p>
+				`
+			})
+			.then((_) => {
+				logger.info('Email', `Email sent to ${to}!`)
+			})
+			.catch((_) => {})
+		}
+	}).catch((_) => {})
 }
 
 /**
@@ -48,22 +56,26 @@ const sendResetPasswordEmail = (to, link) => {
  * @param {object} config
  */
 const sendWatchlistNotificationMessage = (config = { to, asked, price, assetName }) => {
-	sendgrid
-		.send({
-			to: config.to,
-			from: process.env.SENDGRID_EMAIL_SENDER,
-			subject: 'Watchlist Notification!',
-			html: `
-            <h1>Cryptool Service</h1>
-            <p>This is a test! notification.</p>
-            <p>You asked ${config.assetName} to be ${config.asked}. The price has reached the requirement!</p>
-            <p>${config.assetName} is at ${config.price}.</p>
-        `
-		})
-		.then((res) => {
-			logger.info('Email', `Email sent to ${config.to}!`)
-		})
-		.catch((_) => {})
+	ev.isLegitimateEmail(config.to).then((res) => {
+		if (res) {
+			sendgrid
+			.send({
+				to: config.to,
+				from: process.env.SENDGRID_EMAIL_SENDER,
+				subject: 'Watchlist Notification!',
+				html: `
+				<h1>Cryptool Service</h1>
+				<p>This is a test! notification.</p>
+				<p>You asked ${config.assetName} to be ${config.asked}. The price has reached the requirement!</p>
+				<p>${config.assetName} is at ${config.price}.</p>
+			`
+			})
+			.then((_) => {
+				logger.info('Email', `Email sent to ${config.to}!`)
+			})
+			.catch((_) => {})
+		}
+	}).catch((_) => {})
 }
 
 module.exports = {
