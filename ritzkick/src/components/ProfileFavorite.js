27ae7e031b531577ddf2caad296f224ab80d66d3
Hubert <1842014@cegep-ste-foy.qc.ca>
2022-03-02 10:23:42 -0500

    favorite component in profile

--- ritzkick/src/components/ProfileFavorite.js
+++ ritzkick/src/components/ProfileFavorite.js
@@ -2,26 +2,37 @@ import React, { useEffect, useState } from 'react'
 import SimpleCryptoDashboard from '@/components/SimpleCryptoDashboard'
 import { getFavorites } from 'services/UserService'
 import ButtonFavorite from './ButtonFavorite'
+import { SlugArrayToSymbolArray } from 'utils/crypto'
+import { createSocket } from 'services/SocketService'
+import { CircularProgress } from '@mui/material'
+
+const CURRENCY = 'usd'
 
 export default function ProfileFavorite() {
-	const [data, setData] = useState([])
 
-	useEffect(() => {
-		getFavorites()
-			.then((res) => setData(res))
-			.catch((e) => console.log(e))
+	const [socket, setSocket] = useState()
+	const [dateRange, setDateRange] = useState('1d')
+	const [interval, setInterval] = useState('1h')
+
+	useEffect(async () => {
+		const data = await getFavorites()
+		let slugs = []
+		data.map((favorite) => {
+			slugs.push(favorite.slug)
+		})
+		const symbols = SlugArrayToSymbolArray(slugs, CURRENCY, false)
+		setSocket(createSocket(['general', `graph-${dateRange}-${interval}`], symbols))
 	}, [])
 
 	return (
-		<div id="favorites">
-			<h1>Favoris</h1>
-			<ul>
-				{data.map((favorites) => (
-					<li key={favorites._id}>
-						<ButtonFavorite slug={favorites.slug} />
-					</li>
-				))}
-			</ul>
-		</div>
+		 !socket? (
+			<div className='column center'>
+				<CircularProgress color='secondary' />
+			</div>
+		) : 
+			<div id="favorites">
+				<h1>Favoris</h1>
+				<SimpleCryptoDashboard socket={socket} />
+			</div>
 	)
 }
