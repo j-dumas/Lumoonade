fa0ce9815074a5b23aff0a01ff4b3d48a2b68018
Jérémy Dumas <8109955-j-dumas@users.noreply.gitlab.com>
2022-03-08 21:22:11 -0500

    Conflict

--- ritzkick/src/pages/assets.js
+++ ritzkick/src/pages/assets.js
@@ -14,26 +14,25 @@ import { useTranslation } from 'next-i18next'
 const CURRENCY = 'usd'
 
 const Assets = () => {
-	
 	const { t } = useTranslation('assets')
 
 	const [keyword, setKeyword] = useState()
 	const [searchList, setSearchList] = useState([])
 	const [socket, setSocket] = useState()
 	const [favSocket, setFavSocket] = useState()
-	const [pagination, setPagination] = useState([1,1]) // Page#, #Pages
+	const [pagination, setPagination] = useState([1, 1]) // Page#, #Pages
 	const decrementPage = async () => {
 		const currentP = pagination[0]
 		if (currentP > 1) {
-			setPagination(currentP-1, pagination[1])
-			await searchAsset(keyword, currentP-1)
+			setPagination(currentP - 1, pagination[1])
+			await searchAsset(keyword, currentP - 1)
 		}
 	}
 	const incrementPage = async () => {
 		const currentP = pagination[0]
 		if (currentP < pagination[1]) {
-			setPagination(currentP+1, pagination[1])
-			await searchAsset(keyword, currentP+1)
+			setPagination(currentP + 1, pagination[1])
+			await searchAsset(keyword, currentP + 1)
 		}
 	}
 
@@ -94,44 +93,31 @@ const Assets = () => {
 			<section className="sub-section column">
 				{socket ? (
 					<>
-<<<<<<< HEAD
 						{favSocket && searchList.length == 0 ? (
 							<>
 								<div className="row start">
 									<Icons.StarFulled />
 									<h1>{t('favorites')}</h1>
 								</div>
-								<SimpleCryptoCardDashboard socket={favSocket} />
+								{favSocket.auth.query.length == 0 ? (
+									<></>
+								) : (
+									<SimpleCryptoCardDashboard socket={favSocket} />
+								)}
 							</>
-=======
-					{favSocket && searchList.length == 0 ? (
-					<>
-						<div className='row start'>
-							<Icons.StarFulled/>
-							<h1>{t('favorites')}</h1>
-						</div>
-						{favSocket.auth.query.length == 0 ? <></> :
-						<SimpleCryptoCardDashboard socket={favSocket} />}
-					</>
->>>>>>> 475b8f91ca091139ddda84d39644532dd02ce2c2
 						) : null}
 						<div className="row start">
 							<Icons.List />
 							<h1>{t('assets')}</h1>
 						</div>
-<<<<<<< HEAD
 						<SimpleCryptoCardDashboard socket={socket} />
-						{searchList.length > 0 && currentPage[1] > 1 ? (
+						{searchList.length > 0 && pagination[1] > 1 ? (
 							<div className="row center">
 								<button onClick={decrementPage}>{'<'}</button>
-								<p>{currentPage[0]}</p>
+								<p>
+									{pagination[0]} / {pagination[1]}
+								</p>
 								<button onClick={incrementPage}>{'>'}</button>
-=======
-						<SimpleCryptoCardDashboard socket={socket}/>
-						{searchList.length > 0 && pagination[1] > 1 ? (
-							<div className='row center'>
-								<button onClick={decrementPage}>{'<'}</button><p>{pagination[0]} / {pagination[1]}</p><button  onClick={incrementPage}>{'>'}</button>
->>>>>>> 475b8f91ca091139ddda84d39644532dd02ce2c2
 							</div>
 						) : (
 							<></>
